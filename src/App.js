e1fd190994ff45a75d7fe6197dd41c96198cdf5d
Cnfc <stdashkov@mail.ru>
2019-07-08 13:11:08 +0200

    add dinamic messages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-import { db } from "./firebase";
 import Nav from "./Nav";
 import Channel from "./Channel";
 
--- src/ChatInputBox.js
+++ src/ChatInputBox.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { db } from "./firebase";
 
 function ChatInputBox() {
   return (
-    <div className="ChatInputBox">
+    <form
+      onSubmit={event => {
+        event.preventDefault();
+        const value = event.target.elements[0].value;
+        db.collection("channels/random/messages").add({
+          text: value,
+          createApp: new Date()
+        });
+        event.target.reset();
+      }}
+      className="ChatInputBox"
+    >
       <input className="ChatInput" placeholder="Message #general" />
-    </div>
+    </form>
   );
 }
 export default ChatInputBox;
